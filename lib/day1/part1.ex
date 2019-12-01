42f70ac8e4629d0c07660bf94afef93ae086716b
Mathieu Legault <mathieu.legault.5@gmail.com>
2019-12-01 12:29:04 -0500

    Rename method day1

--- lib/day1/part1.ex
+++ lib/day1/part1.ex
@@ -2,11 +2,11 @@ defmodule AdventOfCode.Day1.Part1 do
   def run(input) do
     input
     |> String.split("\n")
-    |> Enum.map(&calculate_fuel_requirement_for_module_mass/1)
+    |> Enum.map(&calculate_fuel_requirement_for_mass/1)
     |> Enum.sum()
   end
 
-  defp calculate_fuel_requirement_for_module_mass(module_mass) do
+  defp calculate_fuel_requirement_for_mass(module_mass) do
     module_mass
     |> String.to_integer()
     |> Kernel./(3)
--- lib/day1/part2.ex
+++ lib/day1/part2.ex
@@ -2,17 +2,17 @@ defmodule AdventOfCode.Day1.Part2 do
   def run(input) do
     input
     |> String.split("\n")
-    |> Enum.map(&calculate_fuel_requirement/1)
+    |> Enum.map(&calculate_fuel_requirement_for_mass/1)
     |> Enum.sum()
   end
 
-  defp calculate_fuel_requirement(mass) when is_binary(mass) do
+  defp calculate_fuel_requirement_for_mass(mass) when is_binary(mass) do
     mass
     |> String.to_integer()
-    |> calculate_fuel_requirement()
+    |> calculate_fuel_requirement_for_mass()
   end
 
-  defp calculate_fuel_requirement(mass) do
+  defp calculate_fuel_requirement_for_mass(mass) do
     fuel_requirement =
       mass
       |> Kernel./(3)
@@ -20,7 +20,7 @@ defmodule AdventOfCode.Day1.Part2 do
       |> Kernel.-(2)
 
     if fuel_requirement >= 6 do
-      fuel_requirement + calculate_fuel_requirement(fuel_requirement)
+      fuel_requirement + calculate_fuel_requirement_for_mass(fuel_requirement)
     else
       fuel_requirement
     end
